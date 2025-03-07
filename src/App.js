4663709da644c580e87b3c84663dd89f1f0235d3
oaeqo <wjdals7898@naver.com>
2025-03-07 16:47:50 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import FAQ from "./components/FAQ";
+import Membership from "./components/Membership";
+import Whyjoin from "./components/Whyjoin";
+import Footer from "./components/Footer";
+import MoveSlider from "./components/--MoveSlider";
+import MoveSlider2 from "./components/MoveSlider2";
+import Login from "./components/Login";
+
+
+const App = () => {
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={
+          <div className="app">
+          <Header />
+          <Banner />
+          {/* <MoveSlider /> */}
+          <MoveSlider2/>
+          <Whyjoin />
+          <FAQ />
+          <Membership />
+          <Footer />
+          </div>
+          }/>
+          <Route path="/login" element={<Login />} />
+    </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import Membership from "./Membership";
+
+const Header = () => {
+  const navigate = useNavigate();
+  return (
+  <header>
+    <nav>
+      <h1>NETFLIX</h1>
+      <div>
+        <select>
+          <option>한국어</option>
+          <option>English</option>
+        </select>
+        <button onClick={()=>{
+          navigate("/login");
+        }}>로그인</button>
+        </div>
+    </nav>
+    <div className="header-content">
+      <h2>영화,시리즈 등을 <br/>무제한으로</h2>
+      <h4>5,500원으로 시작하세요. 멤버십은 언제든지 해지 가능합니다.</h4>
+      <Membership />
+    </div>
+    
+  </header>
+  );
+};
+
+export default Header;
